bbbd3d090d136c1a0588ebc5bcbfe4d93f2366b0
Edward Roche <ed.roche@yahoo.co.uk>
2015-02-17 22:58:20 +0000

    Update Six Nations Function (Need to correctly filter for each team, This is not working)

--- functions.R
+++ functions.R
@@ -17,6 +17,7 @@ getResults <- function(url){
   #Choose which data to keep
   keep = c("Date", "Time (Local)", "Home", "Away", "Score", "Venue")
   results <- results[-1,keep]
+  results$url <- url
   row.names(results) <- NULL
   return(results)
 }
@@ -25,7 +26,7 @@ getResults <- function(url){
 #Format dates, add team and opponent fields and pts diff win lose etc...
 cleanResults <- function(data, team){
   data = cbind(data,data.frame(matrix(as.numeric(unlist(strsplit(data$Score, "-|v"))), ncol=2, byrow=T)))
-  names(data) <- c("date", "time.local", "home.team", "away.team", "score", "venue","home.score", "away.score")
+  names(data) <- c("date", "time.local", "home.team", "away.team", "score", "venue","url", "home.score", "away.score")
   data$date = as.Date(data$date, "%d/%m/%Y")
   data$opponent <- ifelse(data$home.team==team, data$away.team, data$home.team)
   data$home.away <- ifelse(data$home.team==team, "Home", "Away")
@@ -35,7 +36,7 @@ cleanResults <- function(data, team){
   data$month <- as.ordered(month(data$date))
   data$win.lose <- as.factor(ifelse(data$pts.diff>0 , "Win",ifelse(data$pts.diff<0 , "Lose","Draw" ) ))
   data$team <- team
-  keep = c("date", "year", "month","time.local", "team", "opponent", "pts.diff", "home.away", "win.lose")
+  keep = c("date", "year", "month","time.local", "team", "opponent", "pts.diff", "home.away", "win.lose", "url")
   data <- data[,keep]
   return(data)
 }
--- six nations.R
+++ six nations.R
@@ -1,63 +1,10 @@
-library(XML)
-library(lubridate)
-
-rm(list=ls())
+#Get the Six Nations Results since 1882
 
 results <- data.frame(stringsAsFactors=F)
-
-
-
-getData <- function(country) {
-country <- tolower(country)
 #Retrieve data from website
-if (country == "england") { code = 432}
-if (country == "france") { code = 434}
-if (country == "ireland") { code = 436}
-if (country == "italy") { code = 438}
-if (country == "scotland") { code = 440}
-if (country == "wales") { code = 442}
-
-for(j in 2000:2014) {
-#theurl <- paste0('http://www.rbs6nations.com/en/wales/wales_matchcentre.php?includeref=442&season=2010-2011')
-if (country=="wales") {
-  theurl <- paste0('http://www.rbs6nations.com/en/',country,'/',country,'_match_centre.php?includeref=',code,'&season=',j,"-",j+1)
-}
-else {
-  theurl <- paste0('http://www.rbs6nations.com/en/',country,'/',country,'_matchcentre.php?includeref=',code,'&season=',j,"-",j+1)
-}
-html3 <- htmlTreeParse(theurl, useInternalNodes = T)
-date = xpathSApply(html3, "//td[@class='field_DateDmyLong']", xmlValue)
-date = as.Date(date, "%d/%m/%Y")
-time = xpathSApply(html3, "//td[@class='field_TimeLong']", xmlValue)
-home_team = xpathSApply(html3, "//td[@class='field_HomeDisplay']", xmlValue)
-score = xpathSApply(html3, "//td[@class='field_Score']", xmlValue)
-score = data.frame(matrix(as.numeric(unlist(strsplit(score, "-|v"))), ncol=2, byrow=T))
-names(score) <- c("home_score", "away_score")
-away_team = xpathSApply(html3, "//td[@class='field_AwayDisplay']", xmlValue)
-season = paste0(j,"-",j+1)
-team <- capwords(country)
-#Clean data and add useful fields
-opponent <- ifelse(home_team==team, away_team, home_team)
-location <- ifelse(home_team==team, "Home", "Away")
-pts_diff <- ifelse(home_team==team, score$home_score - score$away_score,score$away_score - score$home_score)
-
-year <- as.ordered(year(date))
-result <- as.factor(ifelse(pts_diff>0 , "Win",ifelse(pts_diff<0 , "Lose","Draw" ) ))
-
-results <- rbind(results, data.frame(cbind(season, year, time, team, opponent, pts_diff, location, result, score, stringsAsFactors=F)))
+for(j in 2013:2014) {
+  url = paste0('http://www.rbs6nations.com/en/matchcentre/index.php?includeref=428&season=',j,'-',j+1)
+  results <- rbind(results,getResults(url))
 }
 
-
-
-results <- results[complete.cases(results),]
-}
-
-
-france = getData("France")
-england = getData("England")
-ireland = getData("Ireland")
-wales = getData("Wales")
-scotland = getData("Scotland")
-italy = getData("Italy")
-
-results <- rbind(france, england, ireland, scotland, italy, wales)
+results <- cleanResults(results, "Ireland")
