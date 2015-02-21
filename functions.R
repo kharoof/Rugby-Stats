c5ef2357a8957cee6c3b9e990e1cfb10696f2cf7
Edward Roche <ed.roche@yahoo.co.uk>
2015-02-21 20:13:57 +0000

    Clean the Code and Perform some Visual Analyis

--- functions.R
+++ functions.R
@@ -1,29 +1,9 @@
-##Convert first letter of each word to upper case
-capwords <- function(s, strict = FALSE) {
-  cap <- function(s) paste(toupper(substring(s, 1, 1)),
-{s <- substring(s, 2); if(strict) tolower(s) else s},
-sep = "", collapse = " " )
-sapply(strsplit(s, split = " "), cap, USE.NAMES = !is.null(names(s)))
-}
+library(lubridate)
+####################################################################################################
+## Functions that clean the results for a match
+####################################################################################################
 
-#Function to get results from a webpage
-#Using Xpath and the XML library is one way but the htmltab package should eb more general
-getResults <- function(url){
-  #Get HTML table containing the results
-  #On the rbs website the results are stored in a table with class list but there is a spanning column that contains
-  #the word round which we exclude
-  library(htmltab)
-  results <- htmltab(doc=url, which="//td[@class='list']", body="//tr[not(@class = 'group')]")
-  #Choose which data to keep
-  keep = c("Date", "Time (Local)", "Home", "Away", "Score", "Venue")
-  results <- results[-1,keep]
-  results$url <- url
-  row.names(results) <- NULL
-  return(results)
-}
-
-#Clean the results
-#Format dates, add team and opponent fields and pts diff win lose etc...
+## Format dates, add team and opponent fields and pts diff win lose etc...
 cleanResults <- function(data, team){
   data = cbind(data,data.frame(matrix(as.numeric(unlist(strsplit(data$Score, "-|v"))), ncol=2, byrow=T)))
   names(data) <- c("date", "time.local", "home.team", "away.team", "score", "venue","url", "home.score", "away.score")
@@ -31,7 +11,6 @@ cleanResults <- function(data, team){
   data$opponent <- ifelse(data$home.team==team, data$away.team, data$home.team)
   data$home.away <- ifelse(data$home.team==team, "Home", "Away")
   data$pts.diff <- ifelse(data$home.team==team, data$home.score - data$away.score,data$away.score - data$home.score)
-  library(lubridate)
   data$year <- as.ordered(year(data$date))
   data$month <- as.ordered(month(data$date))
   data$win.lose <- as.factor(ifelse(data$pts.diff>0 , "Win",ifelse(data$pts.diff<0 , "Lose","Draw" ) ))
@@ -43,9 +22,4 @@ cleanResults <- function(data, team){
 
 
 
-#Test functions with real data
-url = 'http://www.rbs6nations.com/en/ireland/ireland_matchcentre.php?includeref=436&season=2010-2011'
-result <- getResults(url)
-
-cleanResults(result, "Ireland")
 
--- six nations.R
+++ six nations.R
@@ -1,10 +1,44 @@
-#Get the Six Nations Results since 1882
+library(ggplot2)
 
-results <- data.frame(stringsAsFactors=F)
-#Retrieve data from website
-for(j in 2013:2014) {
-  url = paste0('http://www.rbs6nations.com/en/matchcentre/index.php?includeref=428&season=',j,'-',j+1)
-  results <- rbind(results,getResults(url))
+####################################################################################################
+## Get the Six Nations Results since 1990
+####################################################################################################
+## Copy the code from the Scrape data repository
+library(XML)
+library(htmltab)
+
+##Function that uses the htmltab function to get results that are stored in tables
+##This extracts the info of interest from each webpage
+parseSixNationsResults <- function(url_html){
+  ##On the rbs website the results are stored in a table with class list but there is a spanning column that contains
+  ##the word round which we exclude
+  results <- htmltab(doc=url_html, which="//td[@class='list']", body="//tr[not(@class = 'group')]")
+  ##Choose which data to keep
+  keep = c("Date", "Time (Local)", "Home", "Away", "Score", "Venue")
+  results <- results[-1,keep]
+  results$url <- docName(url_html)
+  row.names(results) <- NULL
+  return(results)
 }
 
-results <- cleanResults(results, "Ireland")
+#########################
+## Ireland specific results
+#########################
+## Use the XML package to return the html from each page and store this as a list which we will later parse (this will take a long time)
+raw_html_data <- lapply(1990:2014, function(season) htmlTreeParse(paste0('http://www.rbs6nations.com/en/ireland/ireland_matchcentre.php?includeref=436&season=',season,"-",season+1),useInternalNodes=T))
+
+## Extract from the raw html the data that we wish to keep, This will be cleaned later
+ireland <- do.call(rbind,lapply(raw_html_data, parseSixNationsResults))
+
+## Use a custom function to clean the data
+## The general methodology of this funciton to specify a home team, calc pts difference etc is
+## useful and can be reused
+source("functions.R")
+results <- cleanResults(ireland, "Ireland")
+
+## Start with some visual analyis
+p <- ggplot(results, aes(factor(year), pts.diff))
+## Boxplot of the pts difference by year
+p + geom_boxplot() + scale_x_discrete(breaks=c(seq(1995,2015, 4)))
+## Add a facet_wrap
+p + geom_point() + scale_x_discrete(breaks=c(seq(1995,2015, 4))) + facet_wrap(~opponent)
